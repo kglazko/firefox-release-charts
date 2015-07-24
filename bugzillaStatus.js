51acd4a7b249358fdf3d6443cba292ee590a449d
yglazko <yglazko@yglazko.local>
2015-07-24 15:25:00 -0700

    FIXED ALL GLITCHES

--- bugzillaStatus.js
+++ bugzillaStatus.js
@@ -5,8 +5,8 @@
           $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=cf_status_firefox"+version+"&o1=equals&o2=equals&resolution=---&v1=%2B&v2=affected", function(data){
             affected = data.bugs.length;
             if(isReady()==true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          });  
         }
@@ -15,8 +15,8 @@
       $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=cf_status_firefox"+version+"&o1=equals&o2=equals&resolution=---&v1=%2B&v2=unaffected", function(data1){
         unaffected = data1.bugs.length;
         if(isReady()==true){
-         drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-         drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+         drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+         drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          }); 
         }
@@ -26,8 +26,8 @@
       $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=OP&f3=cf_status_firefox"+version+"&j2=OR&known_name=tracking-firefox"+version+"%2B&o1=equals&o3=equals&v1=%2B&v3=fixed&v4=---", function(data2){
      fixed = data2.bugs.length;
           if(isReady() == true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          }); 
         }
@@ -36,8 +36,8 @@
       $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=OP&f3=cf_status_firefox"+version+"&j2=OR&known_name=tracking-firefox"+version+"%2B&o1=equals&o3=equals&v1=%2B&v3=verified&v4=---", function(data3){
       verified = data3.bugs.length;
        if(isReady()==true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
          }
          }); 
         }
@@ -46,30 +46,28 @@
       $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=OP&f3=cf_status_firefox"+version+"&j2=OR&known_name=tracking-firefox"+version+"%2B&o1=equals&o3=equals&v1=%2B&v3=wontfix&v4=---", function(data4){
       wontfix= data4.bugs.length;
       if(isReady()==true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          }); 
         }
 
       function getQuestion(version) {
-      $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox42&f2=OP&f3=cf_status_firefox42&j2=OR&known_name=tracking-firefox42%2B&o1=equals&o3=equals&v1=%2B&v3=?&v4=---", function(data5){
-      alert(data5);
+      $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=OP&f3=cf_status_firefox"+version+"&j2=OR&known_name=tracking-firefox"+version+"%2B&o1=equals&o3=equals&v1=%2B&v3=%3F&v4=---", function(data5){
       question = data5.bugs.length;
-      alert(data5.bugs.length);
     if(isReady()==true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          }); 
         }
 
       function getBlank(version) {
- $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=cf_status_firefox"+version+"&o1=equals&o2=equals&resolution=---&v1=%2B&v2=---", function(data6){
+ $.getJSON("https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&f1=cf_tracking_firefox"+version+"&f2=OP&f3=cf_status_firefox"+version+"&j2=OR&known_name=tracking-firefox"+version+"%2B&o1=equals&o3=equals&v1=%2B&v3=---&v4=---", function(data6){
       blank = data6.bugs.length;
 if(isReady()==true){
-              drawChart(affected,unaffected,fixed,verified,wontfix,blank);
-              drawLabel(affected,unaffected,fixed,verified,wontfix,blank);
+              drawChart(affected,unaffected,fixed,verified,wontfix,question,blank);
+              drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank);
             }
          }); 
         }
--- statusChart.js
+++ statusChart.js
@@ -1,42 +1,50 @@
-function drawChart(affected,unaffected,fixed,verified,wontfix,blank) {
+function drawChart(affected,unaffected,fixed,verified,wontfix, question,blank) {
     document.getElementById('pieHeader').innerHTML = "Tracking " + version + "+ By Status";
     var ctx = document.getElementById("myChart").getContext("2d");
     var data = [
     {
         value: affected,
         color:"#F7464A",
-        highlight: "#FF5A5E",
+        highlight: "#F7464A",
         label: "affected"
     },
     {
         value: unaffected,
         color: "#46BFBD",
-        highlight: "#5AD3D1",
+        highlight: "#46BFBD",
         label: "unaffected"
     },
     {
         value: fixed,
         color: "#FDB45C",
-        highlight: "#FFC870",
+        highlight: "#FDB45C",
         label: "Fixed"
     },
     {
 
         value: verified,
         color: "#0000FF",
-        highlight: "#FFC870",
+        highlight: "#0000FF",
         label: "Verified"
     },
       {
         value: wontfix,
         color: "#FF00FF",
-        highlight: "#FFC870",
+        highlight: "#FF00FF",
         label: "Wontfix"
     },
+
+    {
+        value: question,
+        color: "#336633",
+        highlight: "#336633",
+        label: "?"
+    },
+
         {
         value: blank,
         color: "#C0C0C0",
-        highlight: "#FFC870",
+        highlight: "#C0C0C0",
         label: "---"
     }
 ];
@@ -44,12 +52,13 @@ function drawChart(affected,unaffected,fixed,verified,wontfix,blank) {
     var myPieChart = new Chart(ctx).Pie(data, {legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"});
 }
 
-function drawLabel(affected,unaffected,fixed,verified,wontfix,blank) {
+function drawLabel(affected,unaffected,fixed,verified,wontfix,question,blank) {
     document.getElementById('affected').innerHTML = "Affected: " + affected;
     document.getElementById('unaffected').innerHTML = "Unaffected: " + unaffected;
     document.getElementById('fixed').innerHTML = "Fixed: " + fixed;
     document.getElementById('verified').innerHTML = "Verified: " + verified;
     document.getElementById('wontfix').innerHTML = "Wontfix: " + wontfix;
+    document.getElementById('question').innerHTML = "?: " + question;
     document.getElementById('blank').innerHTML = "---: " + blank;
 }
 
